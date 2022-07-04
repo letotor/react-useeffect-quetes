7008eac74ba11dd1c8bf483fbea8b135003ccedf
victor DG <v.degermain@gmail.com>
2022-07-04 23:05:22 +0200

    ajout des useEffect

--- src/App.js
+++ src/App.js
@@ -2,19 +2,54 @@ import React, { useEffect, useState } from 'react';
 import Message from './Message';
 import './style.css';
 
-export default function App() {
+const   App = ()  =>{
+  const [email, setEmail] = useState('')
+  const [isUserLoggedIn, SetIsUserLoggedIn] = useState(false)
+
+
+  useEffect(()=>{
+    // console.log("email has changed",email)
+  }, [email])
+
+  // useEffect(() => {
+  //   isUserLoggedIn ? console.log("user is logged in") : console.log("user is not loggin")
+
+  // }, [ isUserLoggedIn])
+
   const handleSubmit = e => {
     e.preventDefault();
+    console.log('email submit',email)
+    SetIsUserLoggedIn(true)
   };
+
+
+  const handleChange = e => {
+    setEmail(e.target.value)
+
+  };
+
+
+  const hadnleClick = ()=>{
+    SetIsUserLoggedIn(false)
+
+  }
 
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
         <label htmlFor="email">Email : </label>
-        <input type="text" placeholder="elon@spacex.com" value="" />
-        <input type="submit" value="submit" />
+        <input disabled={isUserLoggedIn} type="text" placeholder="elon@spacex.com" onChange={(e)=>handleChange(e)} />
+        <input disabled={isUserLoggedIn} type="submit" value="submit" />
       </form>
-      <button>Lougout</button>
+    
+    
+      {isUserLoggedIn ? 
+      <>
+        <Message isUserLoggedIn={isUserLoggedIn}/> <button onClick={hadnleClick}>Lougout</button>
+      </>:""}
     </div>
   );
 }
+
+export default App;
+
--- src/Message.js
+++ src/Message.js
@@ -1,5 +1,16 @@
 import React, { useEffect, useState } from 'react';
 
-export default function Message() {
+export default function Message({ isUserLoggedIn }) {
+
+  const sayGoodBy= ()=>{
+    console.log('Bye')
+  }
+  useEffect(() =>{
+   isUserLoggedIn && console.log('user is logged in')
+    return sayGoodBy()
+  }, [])
+
+
+
   return <h1>User is logged in</h1>;
 }
